84361af1c73384ec9b3a2404deb82e651814cb7a
mitrakov <mitrakov-artem@yandex.ru>
2020-07-05 15:35:40 +0300

    Add Markers feature

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,14 @@ class Uber extends StatefulWidget {
 class UberState extends State<Uber> {
   final _geoLocator = Geolocator();
   final CameraPosition _initPos = CameraPosition(target: LatLng(59.9311, 30.3609), zoom: 15.4746);
+  final TextEditingController _startAdderessCtrl = TextEditingController();
+  final TextEditingController _endAdderessCtrl = TextEditingController();
+  final Set<Marker> _markers = Set();
 
-  GoogleMapController _ctrl;
-  //Position _currentPos;
+  GoogleMapController _mapCtrl;
+  Position _currentPos;
+  Position _startPos;
+  Position _endPos;
 
   @override
   Widget build(BuildContext context) {
@@ -40,9 +45,10 @@ class UberState extends State<Uber> {
             zoomGesturesEnabled: true,
             zoomControlsEnabled: false,
             onMapCreated: (GoogleMapController controller) {
-              _ctrl = controller;
+              _mapCtrl = controller;
               moveToCurrentLocation();
             },
+            markers: _markers,
           ),
           Align(
             alignment: Alignment.bottomRight,
@@ -72,7 +78,7 @@ class UberState extends State<Uber> {
                       splashColor: Colors.blue,
                       child: SizedBox(width: 50, height: 50, child: Icon(Icons.add)),
                       onTap: () {
-                        _ctrl.animateCamera(CameraUpdate.zoomIn());
+                        _mapCtrl.animateCamera(CameraUpdate.zoomIn());
                       },
                     ),
                   )
@@ -85,10 +91,25 @@ class UberState extends State<Uber> {
                       splashColor: Colors.blue,
                       child: SizedBox(width: 50, height: 50, child: Icon(Icons.remove)),
                       onTap: () {
-                        _ctrl.animateCamera(CameraUpdate.zoomOut());
+                        _mapCtrl.animateCamera(CameraUpdate.zoomOut());
                       },
                     ),
                   )
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30, top: 30, right: 30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                buildTextField(controller: _startAdderessCtrl, label: "Start", hint: "Start address", prefixIcon: Icon(Icons.looks_one), callback: (s) => findStartAddress()),
+                buildTextField(controller: _endAdderessCtrl, label: "Destination", hint: "Destination address", prefixIcon: Icon(Icons.looks_two), callback: (s) => findDestinationAddress()),
+                RaisedButton(
+                  child: Text("Build route"),
+                  color: Colors.blue[200],
+                  onPressed: () => placeMarkers(),
                 )
               ],
             ),
@@ -101,7 +122,82 @@ class UberState extends State<Uber> {
   void moveToCurrentLocation() async {
     final Position pos = await _geoLocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
     print(pos);
+    _currentPos = pos;
     final position = CameraPosition(target: LatLng(pos.latitude, pos.longitude), zoom: 18);
-    _ctrl.animateCamera(CameraUpdate.newCameraPosition(position));
+    _mapCtrl.animateCamera(CameraUpdate.newCameraPosition(position));
+  }
+
+  void findStartAddress() async {
+    final List<Placemark> placemarks = await _geoLocator.placemarkFromPosition(_currentPos);
+    if (placemarks.isNotEmpty) {
+      final placemark = placemarks.first;
+      final addr = "${placemark.name}, ${placemark.locality}, ${placemark.postalCode}, ${placemark.country}";
+      _startAdderessCtrl.text = addr;
+      _startPos = placemark.position;
+    }
+  }
+
+  void findDestinationAddress() async {
+    final List<Placemark> placemarks = await _geoLocator.placemarkFromAddress(_endAdderessCtrl.text);
+    if (placemarks.isNotEmpty) {
+      final placemark = placemarks.first;
+      print("${placemark.name}, ${placemark.locality}, ${placemark.postalCode}, ${placemark.country}");
+      _endPos = placemark.position;
+    }
+  }
+
+  void placeMarkers() {
+    final startMarker = Marker(
+      markerId: MarkerId("$_startPos"),
+      position: LatLng(_startPos.latitude, _startPos.longitude),
+      infoWindow: InfoWindow(title: "Start", snippet: _startAdderessCtrl.text),
+      icon: BitmapDescriptor.defaultMarker,
+    );
+    final destinationMarker = Marker(
+      markerId: MarkerId("$_endPos"),
+      position: LatLng(_endPos.latitude, _endPos.longitude),
+      infoWindow: InfoWindow(title: "Destination", snippet: _endAdderessCtrl.text),
+      icon: BitmapDescriptor.defaultMarker,
+    );
+
+    setState(() {
+      _markers.addAll([startMarker, destinationMarker]);
+    });
+    zoomCameraForMarkers();
+  }
+
+  void zoomCameraForMarkers() {
+    final sw = _startPos.latitude <= _endPos.latitude ? _startPos : _endPos;
+    final ne = _startPos.latitude <= _endPos.latitude ? _endPos : _startPos;
+
+    final newBounds = LatLngBounds(northeast: LatLng(ne.latitude, ne.longitude), southwest: LatLng(sw.latitude, sw.longitude));
+    _mapCtrl.animateCamera(CameraUpdate.newLatLngBounds(newBounds, 80));
+  }
+
+  Widget buildTextField({TextEditingController controller, String label, String hint, String initValue, Widget prefixIcon, Widget suffixIcon, Function(String) callback}) {
+    return Container(
+      padding: EdgeInsets.only(top: 5, bottom: 5),
+      child: TextField(
+        controller: controller,
+        onChanged: (value) => callback(value),
+        decoration: InputDecoration(
+          prefixIcon: prefixIcon,
+          suffixIcon: suffixIcon,
+          labelText: label,
+          filled: true,
+          fillColor: Colors.white70,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            borderSide: BorderSide(color: Colors.grey[400], width: 2),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            borderSide: BorderSide(color: Colors.blue[300], width: 2),
+          ),
+          contentPadding: EdgeInsets.all(15),
+          hintText: hint,
+        ),
+      ),
+    );
   }
 }
