eff260806023a52d6e985bb7c5b3d1889c3123ac
mitrakov <mitrakov-artem@yandex.ru>
2020-07-13 21:38:12 +0300

    Add styles to address header

--- lib/screens/main/widgets/addressheader.dart
+++ lib/screens/main/widgets/addressheader.dart
@@ -8,14 +8,32 @@ class AddressHeader extends StatelessWidget {
     return ScopedModelDescendant<MapModel>(
       builder: (context1, child, model) {
         return RaisedButton(
+          padding: EdgeInsets.all(0),
           child: Container(
-            height: 75,
+            height: 58,
+            padding: EdgeInsets.only(left: 20, top: 8, bottom: 8),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [BoxShadow(color: Colors.grey.withOpacity(0.2), spreadRadius: 5, blurRadius: 7, offset: Offset(0, 3))]
+            ),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text(model.startAddress?.toShortString() ?? "From"),
-                ListTile(
-                  leading: Icon(Icons.stop),
-                  title: Text(model.destinationAddress?.toShortString() ?? "Where to?"),
+                Padding(
+                  padding: EdgeInsets.only(left: 25),
+                  child: Text("From: ${model.startAddress?.toShortString() ?? ""}", style: TextStyle(fontSize: 12, color: Colors.grey)),
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.stop, size: 12),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 12),
+                        child: Text(model.destinationAddress?.toShortString() ?? "Where to?", style: TextStyle(fontSize: 18)),
+                      ),
+                    )
+                  ],
                 ),
               ],
             ),
